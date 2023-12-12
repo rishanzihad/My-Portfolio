d9e8fce5e88eb5fa25f0969d6c089fdc40a59206
rishanzihad <138320123+rishanzihad@users.noreply.github.com>
2023-12-12 13:32:04 +0600

    contact done

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "./Banner/Banner";
+import Skill from "./Skill/Skill";
 
 
 const Home = () => {
     return (
         <div>
          <Banner></Banner>
+         <Skill></Skill>
         </div>
     );
 };
---
+++ src/Page/Home/Skill/Skill.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Skill = () => {
+    return (
+        <div>
+           
+            <div className='card'>
+            <img className='w-12' src="https://skillicons.dev/icons?i=html" />
+            </div>
+            <p className=' align-middle'>
+                <a >
+                <img src="https://skillicons.dev/icons?i=html,css,js,react,nodejs,express,firebase,mongodb,tailwind,nodejs" />
+                </a>
+            </p>
+        </div>
+    );
+};
+
+export default Skill;
