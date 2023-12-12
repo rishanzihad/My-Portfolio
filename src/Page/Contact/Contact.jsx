b31072cbb74021cdb44cf469207fc794d3aacae1
rishanzihad <138320123+rishanzihad@users.noreply.github.com>
2023-12-12 13:11:56 +0600

    add more information for contact

--- src/Page/Contact/Contact.jsx
+++ src/Page/Contact/Contact.jsx
@@ -2,112 +2,133 @@ import toast from "react-hot-toast";
 
 
 
-
 const Contact = () => {
-   
-    
-    const handleSubmit =  (e) => {
-        e.preventDefault();
-    
-        const form = e.target;
-        const email = form.email.value;
-        const name = form.name.value;
-        const message = form.message.value;
-        const formData = {
-            email,
-            name,
-            message,
-        };
-    
-     
-             fetch('http://localhost:6001/send-email', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(formData),
-            })
-            .then(res => res.json())
-            .then(data => {
-                if(data?.message){
-                    form.reset()
-               
-                    toast.success(`${data.message}`)
-                }
-               
-            })
 
-    
-            
-        } 
- 
-    
-      
-   
-    
-    return (
-        <div >
-        <p>.</p>
-            <h1 className="underline text-4xl mt-20 font-bold text-center">Contact</h1>
-            <div className="flex items-center justify-center">
-            <div className="relative flex flex-col text-gray-700 bg-transparent shadow-none rounded-xl bg-clip-border">
- 
-  <p className="block  text-white font-sans text-base mt-5 antialiased font-normal leading-relaxed">
-    Nice to meet you! Enter your details to Contact With Me.
-  </p>
-  <form onSubmit={handleSubmit} className="max-w-screen-lg mt-8 mb-2 w-80 sm:w-96">
-    <div className="flex flex-col gap-6 mb-1">
-      <h6
-        className="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
-        Your Name
-      </h6>
-      <div className="relative h-11 w-full min-w-[200px]">
-        <input placeholder="Your Name"
-            type="text"
-            name="name"
-           required
-            
-          className="peer text-white h-full w-full rounded-md border border-blue-gray-200 border-t-transparent !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50" />
-        <label
-          className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all before:content-none after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all after:content-none peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"></label>
-      </div>
-      <h6
-        className="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
-        Your Email
-      </h6>
-      <div className="relative h-11 w-full min-w-[200px]">
-        <input type="email"
-        required
-            name="email"
-           placeholder="Your Email"
-          className="peer text-white h-full w-full rounded-md border border-blue-gray-200 border-t-transparent !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50" />
-        <label
-          className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all before:content-none after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all after:content-none peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"></label>
-      </div>
-      <h6
-        className="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
-        Your Message
-      </h6>
-     
-      <textarea name="message"
-      required
-          className="textarea textarea-primary text-white" placeholder="Your Message">
 
-      </textarea>
-    </div>
-   
-    <button
-     className="mt-6 block w-full select-none rounded-lg bg-gray-900 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-      type="submit">
-      Submit
-    </button>
-   
-  </form>
-</div>  
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
+    const form = e.target;
+    const email = form.email.value;
+    const name = form.name.value;
+    const message = form.message.value;
+    const formData = {
+      email,
+      name,
+      message,
+    };
+
+
+    fetch('http://localhost:6001/send-email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data?.message) {
+          form.reset()
+
+          toast.success(`${data.message}`)
+        }
+
+      })
+
+
+
+  }
+
+
+
+
+
+  return (
+    <div className="max-w-[1200px]">
+      <p>.</p>
+      <h1 className="underline text-4xl mt-20 font-bold text-center">Contact</h1>
+      <p className="block text-center  text-white font-sans text-base mt-5 antialiased font-normal leading-relaxed">
+        Nice to meet you! Enter your details to Contact With Me.
+      </p>
+      <div className="md:flex mt-5 px-5 w-full  flex-row justify-center ">
+   
+      <div className="card rounded-r-none bg-gradient-to-r from-cyan-500 to-blue-500 text-primary-content">
+              <div className="card-body">
+                <h2 className="card-title">Card title!</h2>
+                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <div className="card-actions justify-end">
+                  <button className="btn">Buy Now</button>
+                </div>
+              </div>
             </div>
+    
+
+
+         
+
+
+          <div className=" flex-1 rounded-r-xl    flex justify-center  bg-gradient-to-r from-cyan-500 to-blue-500">
+            <div className="relative flex     flex-col text-gray-700 bg-transparent shadow-none rounded-xl bg-clip-border">
+
+
+              <form onSubmit={handleSubmit} className=" mt-5 px-10 mb-2 ">
+                <div className="flex flex-col gap-6 mb-1">
+                  <h6
+                    className="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
+                    Your Name
+                  </h6>
+                  <div className="relative h-11 w-full min-w-[200px]">
+                    <input placeholder="Your Name"
+                      type="text"
+                      name="name"
+                      required
+
+                      className="peer text-white h-full w-full rounded-md border border-blue-gray-200 border-t-transparent !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50" />
+                    <label
+                      className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all before:content-none after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all after:content-none peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"></label>
+                  </div>
+                  <h6
+                    className="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
+                    Your Email
+                  </h6>
+                  <div className="relative h-11 w-full min-w-[350px]">
+                    <input type="email"
+                      required
+                      name="email"
+                      placeholder="Your Email"
+                      className="peer text-white h-full w-full rounded-md border border-blue-gray-200 border-t-transparent !border-t-blue-gray-200 bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50" />
+                    <label
+                      className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all before:content-none after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all after:content-none peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"></label>
+                  </div>
+                  <h6
+                    className="block -mb-3 font-sans text-base antialiased font-semibold leading-relaxed tracking-normal text-blue-gray-900">
+                    Your Message
+                  </h6>
+
+                  <textarea name="message"
+                    required
+                    className="textarea textarea-primary text-white" placeholder="Your Message">
+
+                  </textarea>
+                </div>
+
+                <button
+                  className="mt-6 block w-full select-none rounded-lg bg-gray-900 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                  type="submit">
+                  Submit
+                </button>
+
+              </form>
+            </div>
+
+          </div>
         </div>
-    );
+      </div>
+  
+
+
+  );
 };
 
 export default Contact;
